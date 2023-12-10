c26ee0b08d2a68aa7c51ec5cee35d48999b41d14
Gleyveon <Gleyveon@gmail.com>
2023-12-10 19:07:14 +0100

    Updated Header, Login and Register

--- src/main/java/com/example/redpandaacademy/HelloController.java
+++ src/main/java/com/example/redpandaacademy/HelloController.java
@@ -12,12 +12,12 @@ import java.io.IOException;
 
 public class HelloController {
     @FXML
-    private void onButtonClick(ActionEvent event) throws IOException {
+    private void loginButton(ActionEvent event) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("login.fxml"));
         Parent newTemplate = fxmlLoader.load();
 
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow(); // Fix the case here
-        stage.setScene(new Scene(newTemplate, 800, 600));
+        stage.setScene(new Scene(newTemplate, 1920, 1080));
         stage.show();
     }
 }
