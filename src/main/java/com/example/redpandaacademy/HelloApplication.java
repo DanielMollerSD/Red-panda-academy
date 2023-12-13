3a0e7eb8f70aec736f46829db90e64241b59fb06
BuildTools <unconfigured@null.spigotmc.org>
2023-12-13 13:42:21 +0100

    homepage afgemaakt

--- src/main/java/com/example/redpandaacademy/HelloApplication.java
+++ src/main/java/com/example/redpandaacademy/HelloApplication.java
@@ -12,6 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Homepage.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
+        stage.setFullScreen(true);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
